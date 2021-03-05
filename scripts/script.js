ed2fa84db53e1ba4663d28322459783750609acf
buterbrot359 <batowka359@gmail.com>
2021-03-05 21:00:06 +0600

    asd

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,32 @@
 const left_arrow = document.getElementsByClassName("line_left")[0];
 const line_right = document.getElementsByClassName("line_right")[0];
 const subnav = document.getElementsByClassName("subnav");
+const grid_text = document.getElementsByClassName("grid_text");
 let photo = document.getElementById("slider")
 let num_of_photo = 1;
+const entities = [
+    {
+      city: 'Rostov-on-Don LCD admiral',
+      repair_time: '3.5 months',
+      area:'81 m2',
+      cost: 'Upon request',
+       
+    },
+    {
+      city: 'Sochi Thieves',
+      repair_time: '4 months',
+      area:'105 m2',
+      cost: 'Upon request',
+       
+    },
+    {
+      city: 'Rostov-on-Don Patriotic',
+      repair_time: '3 months',
+      area:'93 m2',
+      cost: 'Upon request',
+      
+    }
+  ]
 left_arrow.addEventListener("click", function() {
     if (num_of_photo == 1) {
         num_of_photo = 3
@@ -45,12 +69,10 @@ function setPhoto() {
     for (v of a) {
         v.src = "images/circle_grey.svg" 
     }
-    if (num_of_photo == 1) {
-        a[0].src = "images/white_circle.svg"
-    } else if (num_of_photo == 2) {
-        a[1].src = "images/white_circle.svg"
-    } else {
-        a[2].src = "images/white_circle.svg"
-    }
+    a[num_of_photo-1].src = "images/white_circle.svg"
+    grid_text[0].innerHTML = entities[num_of_photo-1].city
+    grid_text[1].innerHTML = entities[num_of_photo-1].area
+    grid_text[2].innerHTML = entities[num_of_photo-1].repair_time
+    grid_text[3].innerHTML = entities[num_of_photo-1].cost
     photo.src = `images/slider${num_of_photo}.png`
 }
